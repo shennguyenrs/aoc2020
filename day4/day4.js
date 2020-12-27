2feeee8e02e2b3faa7ffccb0e05ba3e59f647dc7
An Nguyen <shennguyen.redshinobi96@gmail.com>
2020-12-27 19:32:24 +0200

    update code

--- day4/day4.js
+++ day4/day4.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 // Read file to lines
-const lines = fs.readFileSync('entries.txt', 'utf-8').split('\n');
+const lines = fs.readFileSync('entries.txt', 'utf-8').split('\r\n');
 
 const conditions = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
 let fieldsOne = 0;
@@ -10,84 +10,45 @@ let countOne = 0;
 let countTwo = 0;
 let dataLen = lines.length;
 
-const partTwo = (key, value) => {
-  switch (key) {
-    // byr
-    case conditions[0]: {
-      if (Number(value) >= 1920 && Number(value) <= 2002) return true;
-      return false;
-    }
+const validateNumber = (number, min, max) => number >= min && number <= max;
 
-    // iyr
-    case conditions[1]: {
-      if (Number(value) >= 2010 && Number(value) <= 2020) return true;
-      return false;
-    }
+const fieldsValidator = {
+  byr: (value) => validateNumber(Number(value), 1920, 2002),
+  iyr: (value) => validateNumber(Number(value), 2010, 2020),
+  eyr: (value) => validateNumber(Number(value), 2020, 2030),
+  hgt: (value) => {
+    const cm = /^(\d+)cm$/.exec(value);
+    if (cm) return validateNumber(Number(cm[1]), 150, 193);
 
-    // eyr
-    case conditions[2]: {
-      if (Number(value) >= 2020 && Number(value) <= 2030) return true;
-      return false;
-    }
+    const inch = /^(\d+)in/.exec(value);
+    if (inch) return validateNumber(Number(inch[1]), 59, 76);
 
-    // hgt
-    case conditions[3]: {
-      const lenValue = value.length;
-      const unit = value.slice(lenValue - 2, lenValue);
-      const hgt = Number.isInteger(Number(unit))
-        ? Number(value)
-        : Number(value.slice(0, lenValue - 2));
-
-      if (unit === 'cm') {
-        if (hgt >= 150 && hgt <= 193) return true;
-        return false;
-      }
-
-      if (hgt >= 59 && hgt <= 76) return true;
-      return false;
-    }
-
-    // hcl
-    case conditions[4]: {
-      const pattern = /^#([a-f]|[0-9]){6}$/g;
-      return pattern.test(value);
-    }
-
-    // ecl
-    case conditions[5]: {
-      const ecl = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
-      return ecl.includes(value);
-    }
-
-    // pid
-    case conditions[6]: {
-      const pattern = /[0-9]{9}$/g;
-      return pattern.test(value);
-    }
-
-    default:
-      return false;
-  }
+    return false;
+  },
+  hcl: (value) => /^#([0-9a-f]){6}$/.test(value),
+  ecl: (value) => {
+    ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(value);
+  },
+  pid: (value) => /[0-9]{9}$/.test(value),
+  cid: () => false,
 };
 
 // Count Part One
-lines.forEach((line) => {
+lines.slice(0, 13).forEach((line) => {
   const lineLen = line.length;
   dataLen -= 1;
 
-  // console.log(`${lineLen}\t${line}`);
-
-  if (lineLen !== 1) {
+  if (lineLen !== 0) {
     line.split(' ').forEach((token) => {
       const [key, value] = token.split(':');
       fieldsOne += conditions.includes(key) ? 1 : 0;
-      fieldsTwo += partTwo(key, value) ? 1 : 0;
-      // console.log(`${lineLen}\t${key}\t${value}\t\t${partTwo(key, value)}`);
-      // console.log(`${lineLen}\t${key}\t${value}\t${partTwo(key, value)}\t${fieldsTwo}`);
+      fieldsTwo += fieldsValidator[key](value) ? 1 : 0;
+
+      console.log(`${key}\t${value}\t\t${fieldsTwo}`);
     });
   }
 
-  if (lineLen === 1 || dataLen === 0) {
+  if (lineLen === 0 || dataLen === 0) {
     countOne += fieldsOne === 7 ? 1 : 0;
     countTwo += fieldsTwo === 7 ? 1 : 0;
     fieldsOne = 0;
